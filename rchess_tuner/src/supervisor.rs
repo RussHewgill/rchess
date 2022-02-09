c73d8792e986a6885d4f25cf52640427c642b69e
Russ Hewgill <russhewgill@gmail.com>
2022-02-09 09:15:37 -0800

    checkpoint

--- rchess_tuner/src/supervisor.rs
+++ rchess_tuner/src/supervisor.rs
@@ -19,5 +19,17 @@ pub struct Tunable {
     step:    u64,
 }
 
+pub fn run_cutechess(
+    engine1:          Engine,
+    engine2:          Engine,
+    output_label:     &str,
+    num_games:        u64,
+    (elo0,elo1):      (u64,u64),
+    confidence:       f64,
+) {
 
+    let output_file = &format!("tuning_logs/out_{}_{}.pgn", output_label, timestamp);
+    let log_file    = &format!("tuning_logs/log_{}_{}.pgn", output_label, timestamp);
+
+}
 
